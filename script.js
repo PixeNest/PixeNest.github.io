d3aa70b6c87c1a516893cc45d83dbf4f42701c64
mivra <156608502+Mivrabots@users.noreply.github.com>
2024-04-24 20:07:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var nav = document.getElementById('menu');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
